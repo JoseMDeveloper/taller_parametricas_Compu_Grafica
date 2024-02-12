8a4ad6ccc3b826920d82fb21589561df62399f5a
Laura Ovalle Benítez <ovalle.laura@javeriana.edu.co>
2024-02-11 20:53:35 -0500

    punto 2d ready

--- punto2.py
+++ punto2.py
@@ -11,14 +11,22 @@ eps = 2
 xk = np.linspace(-radius, radius, 55)
 x = np.linspace(-radius, radius, 200)
 
+
 def true_fn(x, radius):
-    return np.sqrt(np.where(x <= radius, radius**2 - x**2, np.nan))
+    return np.sqrt(np.where(x <= radius, radius ** 2 - x ** 2, np.nan))
+
+
+def true_fn2(x, radius):
+    return -(np.sqrt(np.where(x <= radius, radius ** 2 - x ** 2, np.nan)))
+
 
 def euclidean_distance(x, xk):
     return np.sqrt(((x.reshape(-1, 1)) - xk.reshape(1, -1)) ** 2)
 
+
 def gauss_rbf(radius, eps):
     return np.exp(-(eps * radius) ** 2)
+
 
 class RBFinterp(object):
     def __init__(self, eps):
@@ -33,9 +41,14 @@ class RBFinterp(object):
         transformation = gauss_rbf(euclidean_distance(xn, self.xk), self.eps)
         return transformation.dot(self.w_)
 
+
 interp = RBFinterp(eps)
 yk = true_fn(xk, radius)
 interp.fit(xk, yk)
+
+interp2 = RBFinterp(eps)
+yk2 = true_fn2(xk, radius)
+interp2.fit(xk, yk2)
 
 pygame.init()
 display = (800, 600)
@@ -70,6 +83,29 @@ while True:
     glColor3f(0, 1, 0)
     for i in range(len(xk)):
         glVertex3f(xk[i], true_fn(xk, radius)[i], 0)
+    glEnd()
+
+    # Dibujar la parte de abajo del circulo
+
+    # Dibujar la función verdadera
+    glBegin(GL_LINE_STRIP)
+    glColor3f(1, 0, 0)
+    for i in range(len(x)):
+        glVertex3f(x[i], true_fn2(x, radius)[i], 0)
+    glEnd()
+
+    # Dibujar la interpolación
+    glBegin(GL_LINE_STRIP)
+    glColor3f(0, 0, 1)
+    for i in range(len(x)):
+        glVertex3f(x[i], interp2(x)[i], 0)
+    glEnd()
+
+    # Dibujar puntos de muestra
+    glBegin(GL_POINTS)
+    glColor3f(1, 1, 0)
+    for i in range(len(xk)):
+        glVertex3f(xk[i], true_fn2(xk, radius)[i], 0)
     glEnd()
 
     pygame.display.flip()
